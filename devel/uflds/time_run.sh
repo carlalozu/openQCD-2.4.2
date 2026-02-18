93bddd5c180b93dea70f371b88e39804bc18728a
Carla Lopez <carlalopez3108@gmail.com>
2026-02-18 17:05:06 +0100

    more thorough script

--- devel/uflds/time_run.sh
+++ devel/uflds/time_run.sh
@@ -18,7 +18,7 @@ base_t1=64
 perl -i -pe "s/#define L0 \\d+/#define L0 $base_t1/" $ROOT/include/global.h
 perl -i -pe "s/#define L0_TRD \\d+/#define L0_TRD $base_t1/" $ROOT/include/global.h
 
-for t in 1 2 4 16
+for t in 1 2 4 8 16
 do
     thread_t1=$((base_t1 / t))
     perl -i -pe "s/#define L0_TRD \\d+/#define L0_TRD $thread_t1/" $ROOT/include/global.h
