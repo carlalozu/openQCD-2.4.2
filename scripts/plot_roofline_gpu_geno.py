9996867ba0b07cebdb3b2d0a4c1776e473e2020c
Carla Lopez <carlalopez3108@gmail.com>
2026-02-21 12:28:42 +0100

    cleanup plot script

--- scripts/plot_roofline_gpu_geno.py
+++ scripts/plot_roofline_gpu_geno.py
@@ -4,29 +4,37 @@ import pandas as pd
 from matplotlib.lines import Line2D
 
 
-# Create the log-log roofline plot
+# Plot parameters
 plt.figure(figsize=(5, 3))
 plt.style.use("seaborn-v0_8-whitegrid")
+markers = Line2D.filled_markers
 
-# add points
-aos_I = 0.7397*2 #flops/byte
-aos_P = 432 #flops
+# Kernel plaqsum
+aos_I = 0.7397  #flops/byte
+aos_P = 432     #flops
 
-# parameters
-threads = [1]
-input_file = "../output/time_gpu.csv"
+# Files
+input_file_gpu = "../output/time_gpu.csv"
+input_file_cpu = "../output/time_threads.csv"
 plot_file = "../output/time_gpu.pdf"
-precision = "float"
 
-perf_1core = 12*2 # in GFlops/s, with AVX on
-memb_1core = 30 # in GB/s
-socket_bw = 460.8 # in GB/s
+# GENO details
+threads = [1, 8, 16]
+perf_1core = 12*2   # in GFlops/s, with AVX on
+memb_1core = 30     # in GB/s
+socket_bw = 460.8   # in GB/s
 
 peak_performances_cpu = [perf_1core*t for t in threads]
 memory_bandwidths_cpu = [memb_1core*t if memb_1core*t<socket_bw else socket_bw for t in threads]
 
-markers = Line2D.filled_markers
 
+# add kernel lines plaq_sum
+plt.vlines(aos_I, 0.001, 1e5, linestyles='dashed', color="black", label="plaq_sum", alpha=0.7, zorder=-1)
+
+
+# Plot cpu roofline and performance
+df_soa = pd.read_csv(input_file_cpu)
+df_soa["op_int"]= aos_I
 for i, t in enumerate(threads):
     x = np.linspace(0.001,  2**10, 100000)
     y = np.minimum(x * memory_bandwidths_cpu[i], peak_performances_cpu[i])
@@ -36,12 +44,16 @@ for i, t in enumerate(threads):
     ridge_x = peak_performances_cpu[i] / memory_bandwidths_cpu[i]
     label_y = peak_performances_cpu[i]*0.5
     plt.text(90, label_y, f"CPU {t} cores", color=line.get_color(), fontsize=9, ha="left", va="bottom")
+    
+    aost = df_soa[df_soa["threads"] == t]
+    plt.scatter(aost["op_int"]+0.005*t, aost["total_mflops"]*1e-3, marker=markers[-i], zorder=4, label=f"{t} cores")
 
 
 # Plot roofline GPU
+df_gpu = pd.read_csv(input_file_gpu)
 labels_gpu = ["FP64 A2000", "FP32 A2000"]
-peak_performances_gpu = np.array([124.8, 7987.2])  # in GFlops/s
-memory_bandwidths_gpu = np.array([288, 288])  # in GB/s
+peak_performances_gpu = np.array([124.8, 7987.2])   # in GFlops/s
+memory_bandwidths_gpu = np.array([288, 288])        # in GB/s
 colors_gpu = ["tab:pink", "tab:brown"]
 for i in range(2):
     x = np.linspace(0.001,  2**10, 100000)
@@ -52,20 +64,13 @@ for i in range(2):
     ridge_x = peak_performances_gpu[i] / memory_bandwidths_gpu[i]
     label_y = peak_performances_gpu[i] * 0.9
     plt.text(90, label_y, labels_gpu[i], color=line.get_color(), fontsize=9, ha="left", va="top")
+plt.scatter(aos_I, df_gpu["total_mflops"]*1e-3, zorder=4, color=colors_gpu[0], label=labels_gpu[0])
 
 
-# add kenrel lines su3matmat
-plt.vlines(aos_I, 0.001, 1e5, linestyles='dashed', colors="black", label="plaq_sum", alpha=0.7, zorder=-1)
-
-df_soa = pd.read_csv(input_file)
-df_soa["op_int"]= aos_I
-for i, t in enumerate(threads):
-    aost = df_soa[df_soa["threads"] == t]
-    plt.scatter(aost["op_int"]+0.005*t, aost["total_mflops"]*1e-3, marker=markers[-i], zorder=4)
-    # print(aost.head(10))
-
+# Problem size label
 for x, y, v in zip(aost["op_int"], aost["total_mflops"]*1e-3, aost["volume"]):
     plt.text(x+0.5,y,str(v),fontsize=9,color="tab:brown",ha="left",va="center")
+
 
 # Add labels and legend
 plt.xlabel('Operational Intensity (FLOPs/Byte)')
@@ -75,6 +80,7 @@ plt.xlim([1e-2, 1e3])
 plt.xscale('log')
 plt.yscale('log')
 plt.legend(fontsize=9)
+
 
 # Show plot
 plt.grid(True)
