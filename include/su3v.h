08ee5fd85ef441ee304ce7e700ec30c13677c247
Carla Lopez <carlalopez3108@gmail.com>
2026-03-16 11:12:55 +0100

    formatting

--- include/su3v.h
+++ include/su3v.h
@@ -1,23 +1,22 @@
 
 /*******************************************************************************
-*
-* File su3v.h
-*
-* Copyright (C) 2026 Carla Lopez
-*
-* This software is distributed under the terms of the GNU General Public
-* License (GPL)
-*
-* Type definitions and macros for vectorized SU(3) matrices, SU(3) vectors
-*
-*******************************************************************************/
+ *
+ * File su3v.h
+ *
+ * Copyright (C) 2026 Carla Lopez
+ *
+ * This software is distributed under the terms of the GNU General Public
+ * License (GPL)
+ *
+ * Type definitions and macros for vectorized SU(3) matrices, SU(3) vectors
+ *
+ *******************************************************************************/
 
 #ifndef SU3V_H
 #define SU3V_H
 
 #include "global.h"
 #include "lattice.h"
-
 
 typedef struct
 {
@@ -50,13 +49,14 @@ typedef struct
 static inline void doublev_init(doublev *x, size_t volume)
 {
     // Round up to nearest 8
-    size_t padded_volume = (volume + 7) & ~7; 
+    size_t padded_volume = (volume + 7) & ~7;
     size_t size = padded_volume * sizeof(double);
     x->volume = padded_volume;
-    x->base = (double*)aligned_alloc(ALIGN, size);
-    if (!x->base) {
+    x->base = (double *)aligned_alloc(ALIGN, size);
+    if (!x->base)
+    {
         x->volume = 0;
-        fprintf(stderr, "Erorr allocating complexv");
+        fprintf(stderr, "Error allocating complexv");
         abort();
     }
 }
@@ -64,22 +64,23 @@ static inline void doublev_init(doublev *x, size_t volume)
 static inline void su3_vec_field_init(su3_vec_field *v, size_t volume)
 {
     // Round up to nearest 8
-    size_t padded_volume = (volume + 7) & ~7; 
+    size_t padded_volume = (volume + 7) & ~7;
     v->volume = padded_volume;
     size_t size = 6 * padded_volume * sizeof(double);
-    v->base = (double*)aligned_alloc(ALIGN, size);
-    if (!v->base) {
+    v->base = (double *)aligned_alloc(ALIGN, size);
+    if (!v->base)
+    {
         v->volume = 0;
-        fprintf(stderr, "Erorr allocating su3_vec_field");
+        fprintf(stderr, "Error allocating su3_vec_field");
         abort();
     }
 
-    v->c1re = v->base + 0*padded_volume;
-    v->c1im = v->base + 1*padded_volume;
-    v->c2re = v->base + 2*padded_volume;
-    v->c2im = v->base + 3*padded_volume;
-    v->c3re = v->base + 4*padded_volume;
-    v->c3im = v->base + 5*padded_volume;
+    v->c1re = v->base + 0 * padded_volume;
+    v->c1im = v->base + 1 * padded_volume;
+    v->c2re = v->base + 2 * padded_volume;
+    v->c2im = v->base + 3 * padded_volume;
+    v->c3re = v->base + 4 * padded_volume;
+    v->c3im = v->base + 5 * padded_volume;
 }
 
 static inline void su3_mat_field_init(su3_mat_field *m_field, size_t volume)
@@ -89,42 +90,43 @@ static inline void su3_mat_field_init(su3_mat_field *m_field, size_t volume)
     su3_vec_field_init(&m_field->c3, volume);
 }
 
-static inline void enter_double_field(doublev* d_field){
-    #pragma omp target enter data \
-    map(to : d_field[0], d_field->base[0:d_field->volume])
+static inline void enter_double_field(doublev *d_field)
+{
+#pragma omp target enter data \
+map(to : d_field[0], d_field->base[0 : d_field->volume])
 }
 
-static inline void enter_su3_mat_field(su3_mat_field* m_field){
-    #pragma omp target enter data map(to: m_field[0]) \
-    map(to: m_field->c1.base[0 : 6*m_field->c1.volume]) \
-    map(to: m_field->c2.base[0 : 6*m_field->c2.volume]) \
-    map(to: m_field->c3.base[0 : 6*m_field->c3.volume])
+static inline void enter_su3_mat_field(su3_mat_field *m_field)
+{
+#pragma omp target enter data map(to : m_field[0])           \
+    map(to : m_field -> c1.base[0 : 6 * m_field->c1.volume]) \
+    map(to : m_field->c2.base[0 : 6 * m_field->c2.volume])   \
+    map(to : m_field->c3.base[0 : 6 * m_field->c3.volume])
 
-    #pragma omp target
+#pragma omp target
     {
-      size_t volume = m_field->c1.volume;
-      m_field->c1.c1re = m_field->c1.base + 0*volume;
-      m_field->c1.c1im = m_field->c1.base + 1*volume;
-      m_field->c1.c2re = m_field->c1.base + 2*volume;
-      m_field->c1.c2im = m_field->c1.base + 3*volume;
-      m_field->c1.c3re = m_field->c1.base + 4*volume;
-      m_field->c1.c3im = m_field->c1.base + 5*volume;
+        size_t volume = m_field->c1.volume;
+        m_field->c1.c1re = m_field->c1.base + 0 * volume;
+        m_field->c1.c1im = m_field->c1.base + 1 * volume;
+        m_field->c1.c2re = m_field->c1.base + 2 * volume;
+        m_field->c1.c2im = m_field->c1.base + 3 * volume;
+        m_field->c1.c3re = m_field->c1.base + 4 * volume;
+        m_field->c1.c3im = m_field->c1.base + 5 * volume;
 
-      m_field->c2.c1re = m_field->c2.base + 0*volume;
-      m_field->c2.c1im = m_field->c2.base + 1*volume;
-      m_field->c2.c2re = m_field->c2.base + 2*volume;
-      m_field->c2.c2im = m_field->c2.base + 3*volume;
-      m_field->c2.c3re = m_field->c2.base + 4*volume;
-      m_field->c2.c3im = m_field->c2.base + 5*volume;
+        m_field->c2.c1re = m_field->c2.base + 0 * volume;
+        m_field->c2.c1im = m_field->c2.base + 1 * volume;
+        m_field->c2.c2re = m_field->c2.base + 2 * volume;
+        m_field->c2.c2im = m_field->c2.base + 3 * volume;
+        m_field->c2.c3re = m_field->c2.base + 4 * volume;
+        m_field->c2.c3im = m_field->c2.base + 5 * volume;
 
-      m_field->c3.c1re = m_field->c3.base + 0*volume;
-      m_field->c3.c1im = m_field->c3.base + 1*volume;
-      m_field->c3.c2re = m_field->c3.base + 2*volume;
-      m_field->c3.c2im = m_field->c3.base + 3*volume;
-      m_field->c3.c3re = m_field->c3.base + 4*volume;
-      m_field->c3.c3im = m_field->c3.base + 5*volume;
+        m_field->c3.c1re = m_field->c3.base + 0 * volume;
+        m_field->c3.c1im = m_field->c3.base + 1 * volume;
+        m_field->c3.c2re = m_field->c3.base + 2 * volume;
+        m_field->c3.c2im = m_field->c3.base + 3 * volume;
+        m_field->c3.c3re = m_field->c3.base + 4 * volume;
+        m_field->c3.c3im = m_field->c3.base + 5 * volume;
     }
 }
-
 
 #endif
--- modules/su3fcts/su3prodv.c
+++ modules/su3fcts/su3prodv.c
@@ -1,29 +1,29 @@
 
 /*******************************************************************************
-*
-* File su3prod.c
-*
-* Copyright (C) 2005, 2009-2013, 2016, 2018, 2021 Martin Luescher
-*
-* This software is distributed under the terms of the GNU General Public
-* License (GPL)
-*
-* Products of double-precision 3x3 matrices in vectorized form
-*
-*   void fsu3matxsu3mat(
-*     const su3_mat_field *u, const su3_mat_field *v, su3_mat_field *res, int n,
-*     int i)
-*   Computes w=u*v assuming that w is different from u.
-* 
-*   void fsu3matdagxsu3matdag(
-*     const su3_mat_field *u, const su3_mat_field *v, su3_mat_field *res, int n,
-*     int i)
-*   Computes w=u^dag*v^dag assuming that w is different from u and v.
-*
-*   void fsu3matxsu3mat_retrace(
-*     const su3_mat_field *u, const su3_mat_field *v, doublev *res, int i)
-*
-*******************************************************************************/
+ *
+ * File su3prod.c
+ *
+ * Copyright (C) 2005, 2009-2013, 2016, 2018, 2021 Martin Luescher
+ *
+ * This software is distributed under the terms of the GNU General Public
+ * License (GPL)
+ *
+ * Products of double-precision 3x3 matrices in vectorized form
+ *
+ *   void fsu3matxsu3mat(
+ *     const su3_mat_field *u, const su3_mat_field *v, su3_mat_field *res, int n,
+ *     int i)
+ *   Computes w=u*v assuming that w is different from u.
+ *
+ *   void fsu3matdagxsu3matdag(
+ *     const su3_mat_field *u, const su3_mat_field *v, su3_mat_field *res, int n,
+ *     int i)
+ *   Computes w=u^dag*v^dag assuming that w is different from u and v.
+ *
+ *   void fsu3matxsu3mat_retrace(
+ *     const su3_mat_field *u, const su3_mat_field *v, doublev *res, int i)
+ *
+ *******************************************************************************/
 
 #ifndef SU3PRODV_C
 #define SU3PRODV_C
@@ -34,26 +34,25 @@
 #include "su3v.h"
 #include "su3fcts.h"
 
-
 #pragma omp declare target
 double fsu3matxsu3mat_retrace(
-   const su3_mat_field *u, const su3_mat_field *v, int i)
+    const su3_mat_field *u, const su3_mat_field *v, int i)
 {
-    double tr_1 = 0.0;
-    double tr_2 = 0.0;
-    double tr_3 = 0.0;
+   double tr_1 = 0.0;
+   double tr_2 = 0.0;
+   double tr_3 = 0.0;
 
-    tr_1 += u->c1.c1re[i] * v->c1.c1re[i] - u->c1.c1im[i] * v->c1.c1im[i];
-    tr_1 += u->c1.c2re[i] * v->c2.c1re[i] - u->c1.c2im[i] * v->c2.c1im[i];
-    tr_1 += u->c1.c3re[i] * v->c3.c1re[i] - u->c1.c3im[i] * v->c3.c1im[i];
+   tr_1 += u->c1.c1re[i] * v->c1.c1re[i] - u->c1.c1im[i] * v->c1.c1im[i];
+   tr_1 += u->c1.c2re[i] * v->c2.c1re[i] - u->c1.c2im[i] * v->c2.c1im[i];
+   tr_1 += u->c1.c3re[i] * v->c3.c1re[i] - u->c1.c3im[i] * v->c3.c1im[i];
 
-    tr_2 += u->c2.c1re[i] * v->c1.c2re[i] - u->c2.c1im[i] * v->c1.c2im[i];
-    tr_2 += u->c2.c2re[i] * v->c2.c2re[i] - u->c2.c2im[i] * v->c2.c2im[i];
-    tr_2 += u->c2.c3re[i] * v->c3.c2re[i] - u->c2.c3im[i] * v->c3.c2im[i];
+   tr_2 += u->c2.c1re[i] * v->c1.c2re[i] - u->c2.c1im[i] * v->c1.c2im[i];
+   tr_2 += u->c2.c2re[i] * v->c2.c2re[i] - u->c2.c2im[i] * v->c2.c2im[i];
+   tr_2 += u->c2.c3re[i] * v->c3.c2re[i] - u->c2.c3im[i] * v->c3.c2im[i];
 
-    tr_3 += u->c3.c1re[i] * v->c1.c3re[i] - u->c3.c1im[i] * v->c1.c3im[i];
-    tr_3 += u->c3.c2re[i] * v->c2.c3re[i] - u->c3.c2im[i] * v->c2.c3im[i];
-    tr_3 += u->c3.c3re[i] * v->c3.c3re[i] - u->c3.c3im[i] * v->c3.c3im[i];
+   tr_3 += u->c3.c1re[i] * v->c1.c3re[i] - u->c3.c1im[i] * v->c1.c3im[i];
+   tr_3 += u->c3.c2re[i] * v->c2.c3re[i] - u->c3.c2im[i] * v->c2.c3im[i];
+   tr_3 += u->c3.c3re[i] * v->c3.c3re[i] - u->c3.c3im[i] * v->c3.c3im[i];
 
    return tr_1 + tr_2 + tr_3;
 }
@@ -61,69 +60,69 @@ double fsu3matxsu3mat_retrace(
 
 #pragma omp declare target
 void fsu3matxsu3mat(
-   const su3_mat_field *u, const su3_mat_field *v, su3_mat_field *res, int n, int i)
+    const su3_mat_field *u, const su3_mat_field *v, su3_mat_field *res, int n, int i)
 {
-    int ip0 = plaq_uidx0(n,i);
-    int ip1 = plaq_uidx1(n,i);
+   int ip0 = plaq_uidx0(n, i);
+   int ip1 = plaq_uidx1(n, i);
 
    //  printf("n: %i, ix: %i, ip: (%i, %i) \n", n, i, ip0, ip1);
 
    res->c1.c1re[i] = u->c1.c1re[ip0] * v->c1.c1re[ip1] - u->c1.c1im[ip0] * v->c1.c1im[ip1] +
-                      u->c1.c2re[ip0] * v->c1.c2re[ip1] - u->c1.c2im[ip0] * v->c1.c2im[ip1] +
-                      u->c1.c3re[ip0] * v->c1.c3re[ip1] - u->c1.c3im[ip0] * v->c1.c3im[ip1];
+                     u->c1.c2re[ip0] * v->c1.c2re[ip1] - u->c1.c2im[ip0] * v->c1.c2im[ip1] +
+                     u->c1.c3re[ip0] * v->c1.c3re[ip1] - u->c1.c3im[ip0] * v->c1.c3im[ip1];
    res->c1.c1im[i] = u->c1.c1re[ip0] * v->c1.c1im[ip1] + u->c1.c1im[ip0] * v->c1.c1re[ip1] +
-                      u->c1.c2re[ip0] * v->c1.c2im[ip1] + u->c1.c2im[ip0] * v->c1.c2re[ip1] +
-                      u->c1.c3re[ip0] * v->c1.c3im[ip1] + u->c1.c3im[ip0] * v->c1.c3re[ip1];
+                     u->c1.c2re[ip0] * v->c1.c2im[ip1] + u->c1.c2im[ip0] * v->c1.c2re[ip1] +
+                     u->c1.c3re[ip0] * v->c1.c3im[ip1] + u->c1.c3im[ip0] * v->c1.c3re[ip1];
    res->c1.c2re[i] = u->c2.c1re[ip0] * v->c1.c1re[ip1] - u->c2.c1im[ip0] * v->c1.c1im[ip1] +
-                      u->c2.c2re[ip0] * v->c1.c2re[ip1] - u->c2.c2im[ip0] * v->c1.c2im[ip1] +
-                      u->c2.c3re[ip0] * v->c1.c3re[ip1] - u->c2.c3im[ip0] * v->c1.c3im[ip1];
+                     u->c2.c2re[ip0] * v->c1.c2re[ip1] - u->c2.c2im[ip0] * v->c1.c2im[ip1] +
+                     u->c2.c3re[ip0] * v->c1.c3re[ip1] - u->c2.c3im[ip0] * v->c1.c3im[ip1];
    res->c1.c2im[i] = u->c2.c1re[ip0] * v->c1.c1im[ip1] + u->c2.c1im[ip0] * v->c1.c1re[ip1] +
-                      u->c2.c2re[ip0] * v->c1.c2im[ip1] + u->c2.c2im[ip0] * v->c1.c2re[ip1] +
-                      u->c2.c3re[ip0] * v->c1.c3im[ip1] + u->c2.c3im[ip0] * v->c1.c3re[ip1];
+                     u->c2.c2re[ip0] * v->c1.c2im[ip1] + u->c2.c2im[ip0] * v->c1.c2re[ip1] +
+                     u->c2.c3re[ip0] * v->c1.c3im[ip1] + u->c2.c3im[ip0] * v->c1.c3re[ip1];
    res->c1.c3re[i] = u->c3.c1re[ip0] * v->c1.c1re[ip1] - u->c3.c1im[ip0] * v->c1.c1im[ip1] +
-                      u->c3.c2re[ip0] * v->c1.c2re[ip1] - u->c3.c2im[ip0] * v->c1.c2im[ip1] +
-                      u->c3.c3re[ip0] * v->c1.c3re[ip1] - u->c3.c3im[ip0] * v->c1.c3im[ip1];
+                     u->c3.c2re[ip0] * v->c1.c2re[ip1] - u->c3.c2im[ip0] * v->c1.c2im[ip1] +
+                     u->c3.c3re[ip0] * v->c1.c3re[ip1] - u->c3.c3im[ip0] * v->c1.c3im[ip1];
    res->c1.c3im[i] = u->c3.c1re[ip0] * v->c1.c1im[ip1] + u->c3.c1im[ip0] * v->c1.c1re[ip1] +
-                      u->c3.c2re[ip0] * v->c1.c2im[ip1] + u->c3.c2im[ip0] * v->c1.c2re[ip1] +
-                      u->c3.c3re[ip0] * v->c1.c3im[ip1] + u->c3.c3im[ip0] * v->c1.c3re[ip1];
+                     u->c3.c2re[ip0] * v->c1.c2im[ip1] + u->c3.c2im[ip0] * v->c1.c2re[ip1] +
+                     u->c3.c3re[ip0] * v->c1.c3im[ip1] + u->c3.c3im[ip0] * v->c1.c3re[ip1];
 
    res->c2.c1re[i] = u->c1.c1re[ip0] * v->c2.c1re[ip1] - u->c1.c1im[ip0] * v->c2.c1im[ip1] +
-                      u->c1.c2re[ip0] * v->c2.c2re[ip1] - u->c1.c2im[ip0] * v->c2.c2im[ip1] +
-                      u->c1.c3re[ip0] * v->c2.c3re[ip1] - u->c1.c3im[ip0] * v->c2.c3im[ip1];
+                     u->c1.c2re[ip0] * v->c2.c2re[ip1] - u->c1.c2im[ip0] * v->c2.c2im[ip1] +
+                     u->c1.c3re[ip0] * v->c2.c3re[ip1] - u->c1.c3im[ip0] * v->c2.c3im[ip1];
    res->c2.c1im[i] = u->c1.c1re[ip0] * v->c2.c1im[ip1] + u->c1.c1im[ip0] * v->c2.c1re[ip1] +
-                      u->c1.c2re[ip0] * v->c2.c2im[ip1] + u->c1.c2im[ip0] * v->c2.c2re[ip1] +
-                      u->c1.c3re[ip0] * v->c2.c3im[ip1] + u->c1.c3im[ip0] * v->c2.c3re[ip1];
+                     u->c1.c2re[ip0] * v->c2.c2im[ip1] + u->c1.c2im[ip0] * v->c2.c2re[ip1] +
+                     u->c1.c3re[ip0] * v->c2.c3im[ip1] + u->c1.c3im[ip0] * v->c2.c3re[ip1];
    res->c2.c2re[i] = u->c2.c1re[ip0] * v->c2.c1re[ip1] - u->c2.c1im[ip0] * v->c2.c1im[ip1] +
-                      u->c2.c2re[ip0] * v->c2.c2re[ip1] - u->c2.c2im[ip0] * v->c2.c2im[ip1] +
-                      u->c2.c3re[ip0] * v->c2.c3re[ip1] - u->c2.c3im[ip0] * v->c2.c3im[ip1];
+                     u->c2.c2re[ip0] * v->c2.c2re[ip1] - u->c2.c2im[ip0] * v->c2.c2im[ip1] +
+                     u->c2.c3re[ip0] * v->c2.c3re[ip1] - u->c2.c3im[ip0] * v->c2.c3im[ip1];
    res->c2.c2im[i] = u->c2.c1re[ip0] * v->c2.c1im[ip1] + u->c2.c1im[ip0] * v->c2.c1re[ip1] +
-                      u->c2.c2re[ip0] * v->c2.c2im[ip1] + u->c2.c2im[ip0] * v->c2.c2re[ip1] +
-                      u->c2.c3re[ip0] * v->c2.c3im[ip1] + u->c2.c3im[ip0] * v->c2.c3re[ip1];
+                     u->c2.c2re[ip0] * v->c2.c2im[ip1] + u->c2.c2im[ip0] * v->c2.c2re[ip1] +
+                     u->c2.c3re[ip0] * v->c2.c3im[ip1] + u->c2.c3im[ip0] * v->c2.c3re[ip1];
    res->c2.c3re[i] = u->c3.c1re[ip0] * v->c2.c1re[ip1] - u->c3.c1im[ip0] * v->c2.c1im[ip1] +
-                      u->c3.c2re[ip0] * v->c2.c2re[ip1] - u->c3.c2im[ip0] * v->c2.c2im[ip1] +
-                      u->c3.c3re[ip0] * v->c2.c3re[ip1] - u->c3.c3im[ip0] * v->c2.c3im[ip1];
+                     u->c3.c2re[ip0] * v->c2.c2re[ip1] - u->c3.c2im[ip0] * v->c2.c2im[ip1] +
+                     u->c3.c3re[ip0] * v->c2.c3re[ip1] - u->c3.c3im[ip0] * v->c2.c3im[ip1];
    res->c2.c3im[i] = u->c3.c1re[ip0] * v->c2.c1im[ip1] + u->c3.c1im[ip0] * v->c2.c1re[ip1] +
-                      u->c3.c2re[ip0] * v->c2.c2im[ip1] + u->c3.c2im[ip0] * v->c2.c2re[ip1] +
-                      u->c3.c3re[ip0] * v->c2.c3im[ip1] + u->c3.c3im[ip0] * v->c2.c3re[ip1];
+                     u->c3.c2re[ip0] * v->c2.c2im[ip1] + u->c3.c2im[ip0] * v->c2.c2re[ip1] +
+                     u->c3.c3re[ip0] * v->c2.c3im[ip1] + u->c3.c3im[ip0] * v->c2.c3re[ip1];
 
    res->c3.c1re[i] = u->c1.c1re[ip0] * v->c3.c1re[ip1] - u->c1.c1im[ip0] * v->c3.c1im[ip1] +
-                      u->c1.c2re[ip0] * v->c3.c2re[ip1] - u->c1.c2im[ip0] * v->c3.c2im[ip1] +
-                      u->c1.c3re[ip0] * v->c3.c3re[ip1] - u->c1.c3im[ip0] * v->c3.c3im[ip1];
+                     u->c1.c2re[ip0] * v->c3.c2re[ip1] - u->c1.c2im[ip0] * v->c3.c2im[ip1] +
+                     u->c1.c3re[ip0] * v->c3.c3re[ip1] - u->c1.c3im[ip0] * v->c3.c3im[ip1];
    res->c3.c1im[i] = u->c1.c1re[ip0] * v->c3.c1im[ip1] + u->c1.c1im[ip0] * v->c3.c1re[ip1] +
-                      u->c1.c2re[ip0] * v->c3.c2im[ip1] + u->c1.c2im[ip0] * v->c3.c2re[ip1] +
-                      u->c1.c3re[ip0] * v->c3.c3im[ip1] + u->c1.c3im[ip0] * v->c3.c3re[ip1];
+                     u->c1.c2re[ip0] * v->c3.c2im[ip1] + u->c1.c2im[ip0] * v->c3.c2re[ip1] +
+                     u->c1.c3re[ip0] * v->c3.c3im[ip1] + u->c1.c3im[ip0] * v->c3.c3re[ip1];
    res->c3.c2re[i] = u->c2.c1re[ip0] * v->c3.c1re[ip1] - u->c2.c1im[ip0] * v->c3.c1im[ip1] +
-                      u->c2.c2re[ip0] * v->c3.c2re[ip1] - u->c2.c2im[ip0] * v->c3.c2im[ip1] +
-                      u->c2.c3re[ip0] * v->c3.c3re[ip1] - u->c2.c3im[ip0] * v->c3.c3im[ip1];
+                     u->c2.c2re[ip0] * v->c3.c2re[ip1] - u->c2.c2im[ip0] * v->c3.c2im[ip1] +
+                     u->c2.c3re[ip0] * v->c3.c3re[ip1] - u->c2.c3im[ip0] * v->c3.c3im[ip1];
    res->c3.c2im[i] = u->c2.c1re[ip0] * v->c3.c1im[ip1] + u->c2.c1im[ip0] * v->c3.c1re[ip1] +
-                      u->c2.c2re[ip0] * v->c3.c2im[ip1] + u->c2.c2im[ip0] * v->c3.c2re[ip1] +
-                      u->c2.c3re[ip0] * v->c3.c3im[ip1] + u->c2.c3im[ip0] * v->c3.c3re[ip1];
+                     u->c2.c2re[ip0] * v->c3.c2im[ip1] + u->c2.c2im[ip0] * v->c3.c2re[ip1] +
+                     u->c2.c3re[ip0] * v->c3.c3im[ip1] + u->c2.c3im[ip0] * v->c3.c3re[ip1];
    res->c3.c3re[i] = u->c3.c1re[ip0] * v->c3.c1re[ip1] - u->c3.c1im[ip0] * v->c3.c1im[ip1] +
-                      u->c3.c2re[ip0] * v->c3.c2re[ip1] - u->c3.c2im[ip0] * v->c3.c2im[ip1] +
-                      u->c3.c3re[ip0] * v->c3.c3re[ip1] - u->c3.c3im[ip0] * v->c3.c3im[ip1];
+                     u->c3.c2re[ip0] * v->c3.c2re[ip1] - u->c3.c2im[ip0] * v->c3.c2im[ip1] +
+                     u->c3.c3re[ip0] * v->c3.c3re[ip1] - u->c3.c3im[ip0] * v->c3.c3im[ip1];
    res->c3.c3im[i] = u->c3.c1re[ip0] * v->c3.c1im[ip1] + u->c3.c1im[ip0] * v->c3.c1re[ip1] +
-                      u->c3.c2re[ip0] * v->c3.c2im[ip1] + u->c3.c2im[ip0] * v->c3.c2re[ip1] +
-                      u->c3.c3re[ip0] * v->c3.c3im[ip1] + u->c3.c3im[ip0] * v->c3.c3re[ip1];
+                     u->c3.c2re[ip0] * v->c3.c2im[ip1] + u->c3.c2im[ip0] * v->c3.c2re[ip1] +
+                     u->c3.c3re[ip0] * v->c3.c3im[ip1] + u->c3.c3im[ip0] * v->c3.c3re[ip1];
 }
 #pragma omp end declare target
 
@@ -132,10 +131,10 @@ void fsu3matxsu3mat(
  */
 #pragma omp declare target
 void fsu3matdagxsu3matdag(
-   const su3_mat_field *u, const su3_mat_field *v, su3_mat_field *res, int n, int i)
+    const su3_mat_field *u, const su3_mat_field *v, su3_mat_field *res, int n, int i)
 {
-   int ip2 = plaq_uidx2(n,i);
-   int ip3 = plaq_uidx3(n,i);
+   int ip2 = plaq_uidx2(n, i);
+   int ip3 = plaq_uidx3(n, i);
 
    // printf("n: %i, ix: %i, ip: (%i, %i) \n", n, i, ip2, ip3);
 
