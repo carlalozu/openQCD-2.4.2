b739de02e076b8f59b1aa93b4c6896647b9269c7
Carla Lopez <carlalopez3108@gmail.com>
2026-06-11 12:07:37 +0200

    change reduction for critical

--- modules/uflds/plaq_sum.c
+++ modules/uflds/plaq_sum.c
@@ -211,8 +211,7 @@ double plaq_action_slices(double *asl)
       rqsmB[t].q[1]=0.0;
    }
 
-#pragma omp parallel private(k,ix,t,n,smE,smB) \
-   reduction(sum_qflt : rqsmE[cpr[0]*L0:L0],rqsmB[cpr[0]*L0:L0])
+#pragma omp parallel private(k,ix,t,n,smE,smB)
    {
       k=omp_get_thread_num();
 
@@ -234,8 +233,11 @@ double plaq_action_slices(double *asl)
                smB+=(3.0-plaq_dble(n,ix));
          }
 
-         acc_qflt(smE,rqsmE[t].q);
-         acc_qflt(smB,rqsmB[t].q);
+#pragma omp critical
+         {
+            acc_qflt(smE,rqsmE[t].q);
+            acc_qflt(smB,rqsmB[t].q);
+         }
       }
    }
 
